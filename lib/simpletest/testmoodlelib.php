2c107f5f43ed925fd47d53e695bb13ba1b209391
Tim Hunt <T.J.Hunt@open.ac.uk>
2010-12-20 16:16:09 +0000

    Current work-in-progress converting the question engine to Moodle 2.0

--- lib/simpletest/testmoodlelib.php
+++ lib/simpletest/testmoodlelib.php
@@ -313,6 +313,10 @@ class moodlelib_test extends UnitTestCase {
             '#()*#,9789\'".,<42897></?$(*DSFMO#$*)(SDJ)($*)');
     }
 
+    function test_clean_param_trim() {
+        $this->assertEqual(clean_param("   Frog   \r\n  ", PARAM_TRIM), 'Frog');
+    }
+
     function test_clean_param_clean() {
         // PARAM_CLEAN is an ugly hack, do not use in new code (skodak)
         // instead use more specific type, or submit sothing that can be verified properly
