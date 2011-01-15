2b3a815c7823df0bfb009e2f90d5b3bb9261f0e7
jamiepratt <me@jamiep.org>
2011-01-15 18:06:50 +0700

    NOBUG New question type code based on ddwtos code with 'ddwtos' replaced by 'sddl' in code and in file names.

---
+++ question/type/sddl/questiontype.php
@@ -0,0 +1,479 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Question type class for the drag-and-drop words into sentences question type.
+ *
+ * @package qtype_sddl
+ * @copyright 2009 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+require_once($CFG->libdir . '/questionlib.php');
+require_once($CFG->dirroot . '/question/engine/lib.php');
+require_once($CFG->dirroot . '/question/format/xml/format.php');
+
+
+/**
+ * The drag-and-drop words into sentences question type class.
+ *
+ * @copyright 2009 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_sddl extends question_type {
+
+    protected function serialize_draggroup_infinite($draggroup, $infinite){
+        $output = new stdClass;
+        $output->draggroup = $draggroup;
+        $output->infinite = $infinite;
+        return serialize($output);
+    }
+
+    public function save_question_options($question) {
+        $result = new stdClass;
+
+        if (!$oldanswers = get_records('question_answers', 'question', $question->id, 'id ASC')) {
+            $oldanswers = array();
+        }
+
+        // Insert all the new answers
+        foreach ($question->choices as $key => $choice) {
+
+            if (trim($choice['answer']) == '') {
+                continue;
+            }
+
+            $feedback = addslashes($this->serialize_draggroup_infinite(
+                    $choice['draggroup'], !empty($choice['infinite'])));
+
+            if ($answer = array_shift($oldanswers)) {  // Existing answer, so reuse it
+                $answer->answer = $choice['answer'];
+                $answer->fraction = 0;
+                $answer->feedback = $feedback;
+                if (!update_record('question_answers', $answer)) {
+                    $result->error = "Could not update drag and drop question answer! (id=$answer->id)";
+                    return $result;
+                }
+            } else {
+                $answer = new stdClass;
+                $answer->answer = $choice['answer'];
+                $answer->question = $question->id;
+                $answer->fraction = 0;
+                $answer->feedback = $feedback;
+                if (!$answer->id = insert_record('question_answers', $answer)) {
+                    $result->error = 'Could not insert drag and drop question answer!';
+                    return $result;
+                }
+            }
+        }
+
+        // Delete old answer records
+        if (!empty($oldanswers)) {
+            foreach($oldanswers as $oa) {
+                delete_records('question_answers', 'id', $oa->id);
+            }
+        }
+
+        $update = true;
+        $options = get_record('question_sddl', 'questionid', $question->id);
+        if (!$options) {
+            $update = false;
+            $options = new stdClass;
+            $options->questionid = $question->id;
+        }
+
+        $options->shuffleanswers = !empty($question->shuffleanswers);
+        $options->correctfeedback = trim($question->correctfeedback);
+        $options->partiallycorrectfeedback = trim($question->partiallycorrectfeedback);
+        $options->shownumcorrect = !empty($question->shownumcorrect);
+        $options->incorrectfeedback = trim($question->incorrectfeedback);
+
+        if ($update) {
+            if (!update_record('question_sddl', $options)) {
+                $result->error = "Could not update question drag and drop wordsintosentences options! (id=$options->id)";
+                return $result;
+            }
+
+        } else {
+            if (!insert_record('question_sddl', $options)) {
+                $result->error = 'Could not insert question drag and drop wordsintosentences options!';
+                return $result;
+            }
+        }
+
+        $this->save_hints($question, true);
+
+        return true;
+    }
+
+    public function get_question_options($question) {
+        // Get additional information from database and attach it to the question object
+        if (!$question->options = get_record('question_sddl', 'questionid', $question->id)) {
+            notify('Error: Missing question options for ou drag and drop wordsintosentences question'.$question->id.'!');
+            return false;
+        }
+
+        parent::get_question_options($question);
+        return true;
+    }
+
+    public function delete_question($questionid) {
+        delete_records('question_sddl', 'questionid', $questionid);
+        return parent::delete_question($questionid);
+    }
+
+    protected function initialise_question_instance(question_definition $question, $questiondata) {
+        parent::initialise_question_instance($question, $questiondata);
+
+        $question->shufflechoices = $questiondata->options->shuffleanswers;
+
+        $question->correctfeedback = $questiondata->options->correctfeedback;
+        $question->partiallycorrectfeedback = $questiondata->options->partiallycorrectfeedback;
+        $question->incorrectfeedback = $questiondata->options->incorrectfeedback;
+        $question->shownumcorrect = $questiondata->options->shownumcorrect;
+
+        $question->choices = array();
+        $choiceindexmap= array();
+
+        // Store the choices in arrays by group.
+        $i = 1;
+        foreach ($questiondata->options->answers as $choicedata) {
+            $options = unserialize($choicedata->feedback);
+            $choice = new qtype_sddl_choice($choicedata->answer,
+                    $options->draggroup, $options->infinite);
+
+            if (array_key_exists($options->draggroup, $question->choices)) {
+                $question->choices[$options->draggroup][] = $choice;
+            } else {
+                $question->choices[$options->draggroup][1] = $choice;
+            }
+
+            end($question->choices[$options->draggroup]);
+            $choiceindexmap[$i] = array($options->draggroup,
+                    key($question->choices[$options->draggroup]));
+            $i += 1;
+        }
+
+        $question->places = array();
+        $question->textfragments = array();
+        $question->rightchoices = array();
+        // Break up the question text, and store the fragments, places and right answers.
+
+        $bits = preg_split('/\[\[(\d+)]]/', $question->questiontext, null, PREG_SPLIT_DELIM_CAPTURE);
+        $question->textfragments[0] = array_shift($bits);
+        $i = 1;
+
+        while (!empty($bits)) {
+            $choice = array_shift($bits);
+
+            list($group, $choiceindex) = $choiceindexmap[$choice];
+            $question->places[$i] = $group;
+            $question->rightchoices[$i] = $choiceindex;
+
+            $question->textfragments[$i] = array_shift($bits);
+            $i += 1;
+        }
+    }
+
+    protected function make_hint($hint) {
+        return question_hint_with_parts::load_from_record($hint);
+    }
+
+    public function get_random_guess_score($questiondata) {
+        $question = $this->make_question($questiondata);
+        return $question->get_random_guess_score();
+    }
+
+    /* This method gets the choices (answers)
+     * in a 2 dimentional array.
+     *
+     * @param object $question
+     * @return array of groups
+     */
+    protected function get_array_of_choices($question) {
+        $subquestions = $question->options->answers;
+        $count = 0;
+        foreach ($subquestions as $key=>$subquestion){
+            $answers[$count]['id'] = $subquestion->id;
+            $answers[$count]['answer'] = $subquestion->answer;
+            $answers[$count]['fraction'] = $subquestion->fraction;
+            $feedback = unserialize( $subquestion->feedback);
+            $answers[$count]['draggroup'] = $feedback->draggroup;
+            $answers[$count]['infinite'] = $feedback->infinite;
+            $answers[$count]['choice'] = $count+1;
+            ++$count;
+        }
+        return $answers;
+    }
+
+    /* This method gets the choices (answers) and sort them by groups
+     * in a 2 dimentional array.
+     *
+     * @param object $question
+     * @return array of groups
+     */
+    protected function get_array_of_groups($question, $state) {
+        $answers = $this->get_array_of_choices($question);
+        $arr = array();
+        for($group=1;$group<count($answers);$group++){
+            $players = $this->get_group_of_players ($question, $state, $answers, $group);
+            if($players){
+                $arr [$group]= $players;
+            }
+        }
+        return $arr;
+    }
+
+    /* This method gets the correct answers in a 2 dimentional array.
+     *
+     * @param object $question
+     * @return array of groups
+     */
+    protected function get_correct_answers($question){
+        $arrayofchoices = $this->get_array_of_choices($question);
+        $arrayofplaceholdeers = $this->get_array_of_placeholders($question);
+
+        $correctplayeers = array();
+        foreach($arrayofplaceholdeers as $ph){
+            foreach($arrayofchoices as $key=>$choice){
+                if(($key+1) == $ph){
+                    $correctplayeers[]= $choice;
+                }
+            }
+        }
+        return $correctplayeers;
+    }
+
+    protected function get_array_of_placeholders($question) {
+        $qtext = $question->questiontext;
+        $error = '<b> ERROR</b>: Please check the form for this question. ';
+        if(!$qtext){
+            echo $error . 'The question text is empty!';
+            return false;
+        }
+
+        //get the slots
+        $slots = $this->getEmbeddedTextArray($question);
+
+        if(!$slots){
+            echo $error . 'The question text is not in the correct format!';
+            return false;
+        }
+
+        $output = array();
+        foreach ($slots as $slot){
+            $output[]=substr($slot, 2, (strlen($slot)-4));//2 is for'[[' and 4 is for '[[]]'
+        }
+        return $output;
+     }
+
+    protected function get_group_of_players ($question, $state, $subquestions, $group){
+        $goupofanswers=array();
+        foreach($subquestions as $key=>$subquestion) {
+            if($subquestion['draggroup'] == $group){
+                $goupofanswers[] =  $subquestion;
+            }
+        }
+
+        //shuffle answers within this group
+        if ($question->options->shuffleanswers == 1) {
+            srand($state->attempt);
+            shuffle($goupofanswers);
+        }
+        return $goupofanswers;
+    }
+
+    public function get_possible_responses($questiondata) {
+        $question = $this->make_question($questiondata);
+
+        $parts = array();
+        foreach ($question->places as $place => $group) {
+            $choices = array();
+
+            foreach ($question->choices[$group] as $i => $choice) {
+                $choices[$i] = new question_possible_response(
+                        html_to_text($question->format_text($choice->text), 0, false),
+                        $question->rightchoices[$place] == $i);
+            }
+            $choices[null] = question_possible_response::no_response();
+
+            $parts[$place] = $choices;
+        }
+
+        return $parts;
+    }
+
+    function import_from_xml($data, $question, $format, $extra=null) {
+        if (!isset($data['@']['type']) || $data['@']['type'] != 'sddl') {
+            return false;
+        }
+
+        $question = $format->import_headers($data);
+        $question->qtype = 'sddl';
+
+        $question->shuffleanswers = $format->trans_single(
+                $format->getpath($data, array('#', 'shuffleanswers', 0, '#'), 1));
+
+        if (!empty($data['#']['dragbox'])) {
+            // Modern XML format.
+            $dragboxes = $data['#']['dragbox'];
+            $question->answer = array();
+            $question->draggroup = array();
+            $question->infinite = array();
+
+            foreach ($data['#']['dragbox'] as $dragboxxml) {
+                $question->choices[] = array(
+                    'answer' => $format->getpath($dragboxxml, array('#', 'text', 0, '#'), '', true),
+                    'draggroup' => $format->getpath($dragboxxml, array('#', 'group', 0, '#'), 1),
+                    'infinite' => array_key_exists('infinite', $dragboxxml['#']),
+                );
+            }
+
+        } else {
+            // Legacy format containing PHP serialisation.
+            foreach ($data['#']['answer'] as $answerxml) {
+                $ans = $format->import_answer($answerxml);
+                $options = unserialize(stripslashes($ans->feedback));
+                $question->choices[] = array(
+                    'answer' => $ans->answer,
+                    'draggroup' => $options->draggroup,
+                    'infinite' => $options->infinite,
+                );
+            }
+        }
+
+        $format->import_combined_feedback($question, $data, true);
+        $format->import_hints($question, $data, true);
+
+        return $question;
+    }
+
+    function export_to_xml($question, $format, $extra = null) {
+        $output = '';
+
+        $output .= '    <shuffleanswers>' . $question->options->shuffleanswers . "</shuffleanswers>\n";
+
+        $output .= $format->write_combined_feedback($question->options);
+
+        foreach ($question->options->answers as $answer) {
+            $options = unserialize($answer->feedback);
+
+            $output .= "    <dragbox>\n";
+            $output .= $format->writetext($answer->answer, 3);
+            $output .= "      <group>{$options->draggroup}</group>\n";
+            if ($options->infinite) {
+                $output .= "      <infinite/>\n";
+            }
+            $output .= "    </dragbox>\n";
+        }
+
+        return $output;
+    }
+
+    /*
+     * Backup the data in the question
+     *
+     * This is used in question/backuplib.php
+     */
+    public function backup($bf, $preferences, $question, $level = 6) {
+        $status = true;
+        $sddl = get_records("question_sddl", "questionid", $question, "id");
+
+        //If there are sddl
+        if ($sddl) {
+            //Iterate over each sddl
+            foreach ($sddl as $ddws) {
+                $status = fwrite ($bf,start_tag("DDWORDSSENTENCES",$level,true));
+                //Print oumultiresponse contents
+                fwrite ($bf,full_tag("SHUFFLEANSWERS",$level+1,false,$ddws->shuffleanswers));
+                fwrite ($bf,full_tag("CORRECTFEEDBACK",$level+1,false,$ddws->correctfeedback));
+                fwrite ($bf,full_tag("PARTIALLYCORRECTFEEDBACK",$level+1,false,$ddws->partiallycorrectfeedback));
+                fwrite ($bf,full_tag("INCORRECTFEEDBACK",$level+1,false,$ddws->incorrectfeedback));
+                fwrite ($bf,full_tag("SHOWNUMCORRECT",$level+1,false,$ddws->shownumcorrect));
+                $status = fwrite ($bf,end_tag("DDWORDSSENTENCES",$level,true));
+            }
+
+            //Now print question_answers
+            $status = question_backup_answers($bf,$preferences,$question);
+        }
+        return $status;
+    }
+
+    /**
+     * Restores the data in the question (This is used in question/restorelib.php)
+     *
+     */
+    public function restore($old_question_id,$new_question_id,$info,$restore) {
+        $status = true;
+
+        //Get the sddl array
+        $sddl = $info['#']['DDWORDSSENTENCES'];
+
+        //Iterate over oumultiresponses
+        for($i = 0; $i < sizeof($sddl); $i++) {
+            $mul_info = $sddl[$i];
+
+            //Now, build the question_sddl record structure
+            $sddl = new stdClass;
+            $sddl->questionid = $new_question_id;
+            $sddl->shuffleanswers = isset($mul_info['#']['SHUFFLEANSWERS']['0']['#'])?backup_todb($mul_info['#']['SHUFFLEANSWERS']['0']['#']):'';
+            if (array_key_exists("CORRECTFEEDBACK", $mul_info['#'])) {
+                $sddl->correctfeedback = backup_todb($mul_info['#']['CORRECTFEEDBACK']['0']['#']);
+            } else {
+                $sddl->correctfeedback = '';
+            }
+            if (array_key_exists("PARTIALLYCORRECTFEEDBACK", $mul_info['#'])) {
+                $sddl->partiallycorrectfeedback = backup_todb($mul_info['#']['PARTIALLYCORRECTFEEDBACK']['0']['#']);
+            } else {
+                $sddl->partiallycorrectfeedback = '';
+            }
+            if (array_key_exists("INCORRECTFEEDBACK", $mul_info['#'])) {
+                $sddl->incorrectfeedback = backup_todb($mul_info['#']['INCORRECTFEEDBACK']['0']['#']);
+            } else {
+                $sddl->incorrectfeedback = '';
+            }
+            if (array_key_exists('SHOWNUMCORRECT', $mul_info['#'])) {
+                $sddl->shownumcorrect = backup_todb($mul_info['#']['SHOWNUMCORRECT']['0']['#']);
+            } else if (array_key_exists('CORRECTRESPONSESFEEDBACK', $mul_info['#'])) {
+                $sddl->shownumcorrect = backup_todb($mul_info['#']['CORRECTRESPONSESFEEDBACK']['0']['#']);
+            } else {
+                $sddl->shownumcorrect = 0;
+            }
+
+            $newid = insert_record ("question_sddl",$sddl);
+
+            //Do some output
+            if (($i+1) % 50 == 0) {
+                if (!defined('RESTORE_SILENTLY')) {
+                    echo ".";
+                    if (($i+1) % 1000 == 0) {
+                        echo "<br />";
+                    }
+                }
+                backup_flush(300);
+            }
+
+            if (!$newid) {
+                $status = false;
+            }
+        }
+        return $status;
+    }
+
+}
---
+++ question/type/sddl/renderer.php
@@ -0,0 +1,221 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Drag-and-drop words into sentences question renderer class.
+ *
+ * @package qtype_sddl
+ * @copyright 2010 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+/**
+ * Generates the output for drag-and-drop words into sentences questions.
+ *
+ * @copyright 2010 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_sddl_renderer extends qtype_with_combined_feedback_renderer {
+    public function formulation_and_controls(question_attempt $qa,
+            question_display_options $options) {
+
+        $question = $qa->get_question();
+
+        $questiontext = '';
+        foreach ($question->textfragments as $i => $fragment) {
+            if ($i > 0) {
+                $questiontext .= $this->drop_box($qa, $i, $options);
+            }
+            $questiontext .= $fragment;
+        }
+
+        $dragboxs = '';
+        foreach ($question->choices as $group => $choices) {
+            $dragboxs .= $this->drag_boxes($qa, $group,
+                    $question->get_ordered_choices($group), $options);
+        }
+
+        $result = '';
+        $result .= html_writer::tag('div', $question->format_text($questiontext),
+                array('class' => 'qtext sddl_questionid_for_javascript', 'id' => $qa->get_qt_field_name('')));
+        $result .= html_writer::tag('div', $dragboxs,
+                array('class' => 'answercontainer'));
+
+        // We abuse the clear_wrong method to output the hidden form fields we
+        // want irrespective of whether we are actually clearing the wrong
+        // bits of the response.
+        if (!$options->clearwrong) {
+            $result .= $this->clear_wrong($qa, false);
+        }
+
+        if ($qa->get_state() == question_state::$invalid) {
+            $result .= html_writer::nonempty_tag('div',
+                    $question->get_validation_error($qa->get_last_qt_data()),
+                    array('class' => 'validationerror'));
+        }
+
+        return $result;
+    }
+
+    /**
+     * Modify the contents of a drag/drop box to fix some IE-related problem.
+     * Unfortunately I don't have more details than that.
+     * @param string $string the box contents.
+     * @return string the box contents modified.
+     */
+    protected function dodgy_ie_fix($string) {
+        return '<sub>&#160;</sub>' . $string . '<sup>&#160;</sup>';
+    }
+
+    protected function drop_box(question_attempt $qa, $place, question_display_options $options) {
+        $question = $qa->get_question();
+        $group = $question->places[$place];
+        $boxcontents = $this->dodgy_ie_fix('&#160;');
+
+        $value = $qa->get_last_qt_var($question->field($place));
+
+        $attributes = array(
+            'id' => $this->box_id($qa, 'p' . $place, $group),
+            'class' => 'slot group' . $group
+        );
+
+        if ($options->readonly) {
+            $attributes['class'] .= ' readonly';
+        } else {
+            $attributes['tabindex'] = '0';
+        }
+
+        $feedbackimage = '';
+        if ($options->correctness) {
+            $response = $qa->get_last_qt_data();
+            $fieldname = $question->field($place);
+            if (array_key_exists($fieldname, $response)) {
+                $fraction = (int) ($response[$fieldname] == $question->get_right_choice_for($place));
+                $attributes['class'] .= ' ' . $this->feedback_class($fraction);
+                $feedbackimage = $this->feedback_image($fraction);
+            }
+        }
+
+        return html_writer::tag('span', $boxcontents, $attributes) . ' ' . $feedbackimage;
+    }
+
+    protected function drag_boxes($qa, $group, $choices, question_display_options $options) {
+        $readonly = '';
+        if ($options->readonly) {
+            $readonly = ' readonly';
+        }
+
+        $boxes = '';
+        foreach ($choices as $key => $choice) {
+            //Bug 8632 -  long text entry causes bug in drag and drop field in IE
+            $content = str_replace('-', '&#x2011;', $choice->text);
+            $content = $this->dodgy_ie_fix(str_replace(' ', '&#160;', $content));
+
+            $infinite = '';
+            if ($choice->isinfinite) {
+                $infinite = ' infinite';
+            }
+
+            $boxes .= html_writer::tag('span', $content, array(
+                    'id' => $this->box_id($qa, $key, $choice->draggroup),
+                    'class' => 'player group' . $choice->draggroup . $infinite . $readonly)) . ' ';
+        }
+
+        return html_writer::nonempty_tag('div', $boxes, array('class' => 'answertext'));
+    }
+
+    protected function box_id(question_attempt $qa, $place, $group) {
+        return $qa->get_qt_field_name($place) . '_' . $group;
+    }
+
+    public function specific_feedback(question_attempt $qa) {
+        return $this->combined_feedback($qa);
+    }
+
+    public function head_code(question_attempt $qa) {
+        require_js(array('yui_dom-event', 'yui_dragdrop'));
+        return parent::head_code($qa);
+    }
+
+    /**
+     * Actually, this question type abuses this method to always ouptut the
+     * hidden fields it needs.
+     */
+    public function clear_wrong(question_attempt $qa, $reallyclear = true) {
+        $question = $qa->get_question();
+        $response = $qa->get_last_qt_data();
+
+        if (!empty($response) && $reallyclear) {
+            $cleanresponse = $question->clear_wrong_from_response($response);
+        } else {
+            $cleanresponse = $response;
+        }
+
+        $output = '';
+        foreach ($question->places as $place => $group) {
+            $fieldname = $question->field($place);
+            if (array_key_exists($fieldname, $response)) {
+                $value = $response[$fieldname];
+            } else {
+                $value = '0';
+            }
+            if (array_key_exists($fieldname, $cleanresponse)) {
+                $cleanvalue = $cleanresponse[$fieldname];
+            } else {
+                $cleanvalue = '0';
+            }
+            if ($cleanvalue != $value) {
+                $output .= html_writer::empty_tag('input', array(
+                        'type' => 'hidden',
+                        'id' => $this->box_id($qa, 'p' . $place, $group) . '_hidden',
+                        'value' => s($value))) .
+                        html_writer::empty_tag('input', array(
+                        'type' => 'hidden',
+                        'name' => $qa->get_qt_field_name($fieldname),
+                        'value' => s($cleanvalue)));
+            } else {
+                $output .= html_writer::empty_tag('input', array(
+                        'type' => 'hidden',
+                        'id' => $this->box_id($qa, 'p' . $place, $group) . '_hidden',
+                        'name' => $qa->get_qt_field_name($fieldname),
+                        'value' => s($value)));
+            }
+        }
+        return $output;
+    }
+
+    public function correct_response(question_attempt $qa) {
+        $question = $qa->get_question();
+
+        $correctanswer = '';
+        foreach ($question->textfragments as $i => $fragment) {
+            if ($i > 0) {
+                $group = $question->places[$i];
+                $choice = $question->choices[$group][$question->rightchoices[$i]];
+                $correctanswer .= '[' . str_replace('-', '&#x2011;',
+                        $choice->text) . ']';
+            }
+            $correctanswer .= $fragment;
+        }
+
+        if (!empty($correctanswer)) {
+            return get_string('correctansweris', 'qtype_sddl', $correctanswer);
+        }
+    }
+}
