75a5065292369ed24fc8d515a043175cc3485fe5
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-01-27 17:55:28 +0000

    Bug 11408 JME and applet components don't work in IE.

--- question/type/opaque/locallib.php
+++ question/type/opaque/locallib.php
@@ -594,6 +594,16 @@ function extract_stuff_from_response(&$opaquestate, $response, &$resourcecache) 
     }
 
     // Process the resources.
+    // TODO remove this. Evil hack. IE cannot cope with : and other odd characters
+    // in the name argument to window.open. Until we can deploy a fix to the
+    // OpenMark servers, apply the fix to the JS code here.
+    foreach ($response->resources as $key => $resource) {
+        if ($resource->filename == 'script.js') {
+            $response->resources[$key]->content = preg_replace(
+                    '/(?<=window\.open\(\"\", idprefix|window\.open\(\"\",idprefix)\+(?=\"\w+\"\+id,)/',
+                    '.replace(/\W/g,"_")+', $resource->content);
+        }
+    }
     $resourcecache->cache_resources($response->resources);
 
     // Process the other bits.
